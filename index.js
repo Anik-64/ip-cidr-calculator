03f2aa126220c9a60eb264dc16427d87ca4e027a
Anik Majumder <101924875+Anik-64@users.noreply.github.com>
2025-04-06 00:25:09 +0600

    Update:/ Time limit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
+  windowMs: 60 * 60 * 1000,
   max: 100,
   message: "Too many requests from this IP, please try again later.",
   standardHeaders: true,
